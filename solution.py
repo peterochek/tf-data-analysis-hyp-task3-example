8af6227f1812d4d656b94c70b02ce53492b74e0f
Peter Korolev <peterkkor@gmail.com>
2024-05-06 22:18:34 +0300

    init

--- solution.py
+++ solution.py
@@ -2,10 +2,20 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 285458518 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def test_profitability_threshold(profit_data):
+    sample_mean = np.mean(profit_data)
+    sample_std = np.std(profit_data, ddof=1)
+    n = len(profit_data)
+    
+    t_stat = (sample_mean - 500) / (sample_std / np.sqrt(n))
+    
+    critical_t_value = 1.286
+    
+    reject_null = t_stat > critical_t_value
+    
+    return reject_null
+
+def solution(x) -> bool:
+    return test_profitability_threshold(x)
